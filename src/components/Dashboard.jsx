8824343561b3ac3859f930ffcd336dc31f25720f
chinnarasu-kanthan <crasu341@gmail.com>
2022-02-14 08:37:33 +0530

    1 file modified

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, {  useState, useEffect } from 'react';
 import {  useSelector } from 'react-redux';
-import { useStore } from 'react-redux'
+
 
 const importView = (layout) =>
   React.lazy(() => import("./"+layout).catch((e) => import('./Layout')));
@@ -8,18 +8,17 @@ const importView = (layout) =>
 const Dashboard = () => {
   const [views, setViews] = useState([]);
   const { user: currentUser } = useSelector((state) => state.auth);
-  const store = useStore()
+
   
   useEffect(() => {
-    const user = store.getState().auth;
-   let cuser = JSON.parse(JSON.stringify(user.user));
-      let layout =cuser ? cuser.layout:"Layout";
+  
+      let layout =currentUser ? currentUser.layout:"Layout";
   async function loadViews() {
           const View = await importView(layout);
       Promise.resolve(<View />).then(setViews);
     }
    loadViews();
-  }, [store]);
+  }, [currentUser]);
 
   return (
     <React.Suspense fallback="Loading views...">
